70d70a2e6b814123110b57ef71e2dc19fe75390a
xuncheng <205455603@qq.com>
2025-11-05 19:20:55 +0800

    Finish the map,add something aboout move_base.

---
+++ build/gazebo_map/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/xuncheng/Gazebo-Inspection/src/gazebo_map
+# Build directory: /home/xuncheng/Gazebo-Inspection/build/gazebo_map
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/gazebo_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/xuncheng/Gazebo-Inspection/src/gazebo_pkg
+# Build directory: /home/xuncheng/Gazebo-Inspection/build/gazebo_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
